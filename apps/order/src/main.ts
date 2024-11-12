7487276ac1aa94afafff33562d495ab46455bea8
pakobarbakadze <pakobarbakadze@gmail.com>
2024-11-12 14:21:06 +0400

    build: gRPC

--- apps/order/src/main.ts
+++ apps/order/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { ORDER_PACKAGE_NAME } from '../../../libs/shared/src/proto/order';
 import { OrderModule } from './order.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(OrderModule);
-  await app.listen(process.env.port ?? 3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    OrderModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: ORDER_PACKAGE_NAME,
+        protoPath: join(__dirname, '../order.proto'),
+        url: '0.0.0.0:50051',
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
